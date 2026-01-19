a62304efc9450cf4ca887ab731005c9db5fdb268
CR0NYM3X <88253386+CR0NYM3X@users.noreply.github.com>
2026-01-19 00:38:57 -0700

    Update notice_color.sql

--- Colors/notice_color.sql
+++ Colors/notice_color.sql
@@ -1,6 +1,6 @@
 
 /*
-Permite manejar los logs a otro nivel, guardandolo en su propia tabla, propio archivo de log a nivel S.O incluso mostrando el mensaje personalizado en pantalla  
+Permite manejar los logs a otro nivel, guardandolo en su propia tabla, propio archivo de log a nivel S.O en una carpeta especifica y con fecha del log  incluso mostrando el mensaje personalizado en pantalla  
 
 
 FUNCION QUE TE PERMITE AGREGARLE COLOR AL TEXTO
@@ -245,4 +245,5 @@ SELECT notice_color('TEXT TRANSFORMADO BOLD' , '', '' ,TRUE  ,NULL, false, 'lowe
  
  
  
+
 
